fc96660563e1a75b9983c7592e9997ae8f3f89c7
zhangfan <569873962@qq.com>
2019-03-03 16:40:07 +0800

    跳一跳

--- JS/Lobby.js
+++ JS/Lobby.js
@@ -1,6 +1,17 @@
-let a = 1;
+function init() {
+    document.getElementById('play').onmousedown = function () {
+        var timer = setInterval(function () {
+            time++;
+        }, 100);
+        alert('you lost');
+        document.getElementById('play').onmouseup = function () {
+            alert('you lost');
+        };
+    };
+}
 function button() {
     if (a == 2) {
         console.log("异常!!");
     }
 }
+init();
